278cb5c9a7808c4a2d97163b45bb403f7efb262a
Ryan Wilson <ryanswilson1@gmail.com>
2020-10-28 12:26:51 -0400

    updated htmlroutes.js

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -11,8 +11,4 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-
 };
