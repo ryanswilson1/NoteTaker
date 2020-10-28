7230b87ccf43f8ad220041ec20f1038ef3461982
Ryan Wilson <ryanswilson1@gmail.com>
2020-10-28 18:29:27 -0400

    updated apiRoute name

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.static("/assets"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/apiRoute")(app);
+require("./routes/htmlRoute")(app);
 
 app.listen(PORT, () => console.log("Server is running on " + PORT));
